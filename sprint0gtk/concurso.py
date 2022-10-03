7baddceede4b3a9591a12fba2ba3a500350ca8f4
Brunoo652 <breboredoa@fpcoruna.afundacion.org>
2022-10-03 17:39:23 +0200

    Ejercicios 1 y 2 del sprint 0

---
+++ sprint0gtk/concurso.py
@@ -0,0 +1,54 @@
+#import os
+
+opcA="A"
+opcB="B"
+opcC="C"
+result=" " 
+puntuFinal=0
+puntoPos=10
+puntuNeg=5
+
+print("Responde a la siguiente pregunta (1/3): ")
+print("De que color es el cielo?")
+print("A.-Amarillo")
+print("B.-Azul")
+print("C.-Verde")
+result= input()
+while result != opcB :
+	print("Respuesta incorrecta. Prueba otra opcion")
+	puntuFinal= puntuFinal-puntuNeg
+	result= input()
+	
+print("Respuesta Correcta")
+puntuFinal=puntuFinal+puntoPos
+
+
+print("Responde a la siguiente pregunta (2/3): ")
+print("Cuanto es 2+2?")
+print("A.-4")
+print("B.-5")
+print("C.-1")
+while result != opcA :
+	print("Respuesta incorrecta. Prueba otra opcion")
+	puntuFinal= puntuFinal-puntuNeg
+	result= input()
+
+print("Respuesta Correcta")
+puntuFinal=puntuFinal+puntoPos
+
+print("Responde a la siguiente pregunta (3/3): ")
+print("En que año se inició la revolución francesa?")
+print("A.-1492")
+print("B.-1910")
+print("C.-1789")
+result= input()
+while result != opcC :
+	print("Respuesta incorrecta. Prueba otra opcion")
+	puntuFinal= puntuFinal-puntuNeg
+	result= input()
+	
+print("Respuesta Correcta")
+puntuFinal=puntuFinal+puntoPos
+
+
+#print("Puntuación final: "+puntuFinal)
---
+++ sprint0gtk/fibonacci.py
@@ -0,0 +1,11 @@
+class funcion_fibonacci():
+     if n == 0:
+      #  return 0
+    if n == 1:
+      #  return 1
+ #   return funcion_fibonacci(n-1) + funcion_fibonacci(n-2)
+
+if __name__ == '__main__':
+    funcion_fibonacci = funcion_fibonacci(10)
+    print('Fibonacci de orden 10:')
+    print(funcion_fibonacci)

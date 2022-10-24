e7d402df34197e3cbacc414584c8dc19d23d6874
Brunoo652 <breboredoa@fpcoruna.afundacion.org>
2022-10-24 12:09:33 +0200

    Ejercicio 2 sprint 3

--- sprint2catalog/catalog/main.py
+++ sprint2catalog/catalog/main.py
@@ -1,6 +1,13 @@
 import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
+from load_window import LoadWindow
 
 class MainWIndow(Gtk.window):
     image=Gtk.image
+
+
+win = LoadWindow()
+win.show_all()
+
+Gtk.main()

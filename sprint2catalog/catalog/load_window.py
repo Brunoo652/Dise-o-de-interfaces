cf8504cf041622663ca31eff9b17ee0bc75084dc
Brunoo652 <breboredoa@fpcoruna.afundacion.org>
2022-10-25 09:23:18 +0200

    Carpeta API Rest y ejercicio 3 incpmpleto

--- sprint2catalog/catalog/load_window.py
+++ sprint2catalog/catalog/load_window.py
@@ -1,6 +1,12 @@
+import shutil
+import threading
+
 import gi
-gi.require_version ("Gtk", "3.0")
-from gi.repository import Gtk
+import requests
+import thread
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk, GLib
+
 
 class LoadWindow(Gtk.Window):
     label = Gtk.Label("Cargando elementos...")
@@ -17,3 +23,23 @@ class LoadWindow(Gtk.Window):
         self.box.pack_start(self.label, False, False, 0)
         self.box.pack_start(self.spinner, False, False, 0)
         self.add(self.box)
+
+    def load_json(self):
+        response = requests.get('https://github.com/Brunoo652/Interfaces/blob/main/sprint2catalog/catalog/data/edited/serie3.png')
+        josn_list = response.json()
+
+        result = []
+
+        for json_item in json_list:
+            name = json_item("name")
+            description = json_item.get("image_url")
+            r = requests.get(image_url, stream=True)
+            with open("serie3.png", "wb"):
+                shutil.copyfileobj(r.raw, f)
+            image = Gtk.Image.new_from_file("serie3.png")
+            result.append{"name": name, "description": description, "gtk_image": image}
+    def launch_load(self):
+        thread = threading.Thread(target=self.load.json, args=())
+        thread.start()
+
+   # def load_
